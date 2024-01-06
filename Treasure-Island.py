cb01a6abbe18d9dec122208e2d182426efb466ef
Martin Ferrero-Thompson <153296438+mft-web-designs@users.noreply.github.com>
2024-01-06 14:47:56 +0100

    Create Treasure-Island.py
    Treasure Island Challenge complete

---
+++ Treasure-Island.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("You are at a crossroads. To the left is a dark forest, to the right is a beautiful beach.")
+direction = input("Which way do you want to go? Left or Right? ")
+direction = direction.lower()
+if direction == "right":
+  print("There is no Right... Only your mind makes you think so... Game Over.")
+else:
+  print("You arrive at a beach. You see a small island in the distance. You can either swim to the island or wait for a boat.")
+  action = input("What do you want to do? Swim or Wait? ")
+  action = action.lower()
+  if action == "swim":
+    print("Swimming in a sea of sharks is not a not a wise choice... Game Over.")
+  else:
+    print("You arrive at the island. You see a house with three doors. One red, one yellow, and one blue.")
+    door = input("Which door do you want to open? Red, Yellow, or Blue? ")
+    door = door.lower()
+    if door == "yellow":
+      print("You Win!")
+    else:
+      print("You never learn, do you? Game Over.")
