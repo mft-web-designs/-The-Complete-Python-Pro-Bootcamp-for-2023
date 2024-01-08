6c75ea4750af8517810533a17aeee4103cf7f7af
Martin Ferrero-Thompson <153296438+mft-web-designs@users.noreply.github.com>
2024-01-08 09:38:24 +0100

    Create FizzBuzz.py
    Good old fizzbuzz code snippet

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+# Write your code here 👇
+
+start_number = 1
+end_number = 100
+
+for number in range(start_number, end_number + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
