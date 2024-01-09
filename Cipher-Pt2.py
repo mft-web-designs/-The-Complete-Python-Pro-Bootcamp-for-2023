37e03289b180749a4b526bbd55c7cef183b480ea
Martin Ferrero-Thompson <153296438+mft-web-designs@users.noreply.github.com>
2024-01-09 11:53:24 +0100

    Create Cipher-Pt2.py
    Cipher Code snippet

---
+++ Cipher-Pt2.py
@@ -0,0 +1,37 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+#TODO: Check that the shift number is >1 and <25
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            if new_position > 25:
+                new_position = new_position - 25
+            cipher_text += alphabet[new_position]
+        else:
+            cipher_text += letter
+    print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text, shift_amount):
+  
+    plain_text = ""
+    for letter in cipher_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position - shift_amount
+            if new_position < 0:
+                new_position = new_position + 25
+            plain_text += alphabet[new_position]
+        else:
+            plain_text += letter
+    print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(cipher_text=text, shift_amount=shift)
